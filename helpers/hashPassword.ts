0f4cfe412279ad0ba148a6244d5bcfcaadd6f953
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-02 23:18:46 +0100

    fix: await hashedpwd

--- helpers/hashPassword.ts
+++ helpers/hashPassword.ts
@@ -2,6 +2,6 @@ import bcrypt from "bcrypt"
 
 export const getHashedPassword = async(password:string)=>{
     const salt = await bcrypt.genSalt(12)
-    const hashedPassword = bcrypt.hash(password, salt)
+    const hashedPassword = await bcrypt.hash(password, salt)
     return {salt, hashedPassword}
 }

224820653ed6cb0b7271c6d458fe78f375ecf93a
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-02 21:37:39 +0100

    models: users model initial commit

---
+++ models/users.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import { IUserDoc } from "../types";
+
+export const User = mongoose.model<IUserDoc>(
+  "Users",
+  new mongoose.Schema<IUserDoc>({
+    username: {
+        //Will be a combination of first and last name
+      type: String,
+      minlength: 2,
+      maxlength: 255,
+      required: true,
+    },
+    email: {
+      type: String,
+      minlength: 5,
+      maxlength: 255,
+      required: true,
+      unique: true
+    },
+    phone: {
+      type: String,
+      minlength: 10,
+      maxlength: 20,
+      required: true,
+      unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 255
+    },
+    role: {
+        type: String,
+        enum: ["User", "Admin"],
+        default: "User"
+    }
+  })
+);

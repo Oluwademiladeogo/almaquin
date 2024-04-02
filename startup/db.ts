7bf37f24bd0fdf22a203f0a50d3d11afa80129c3
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-02 23:29:04 +0100

    code refactor

--- startup/db.ts
+++ startup/db.ts
@@ -1,21 +1,20 @@
-import { config } from 'dotenv';
-import mongoose from 'mongoose';
+import { config } from "dotenv";
+import mongoose from "mongoose";
 config();
 
-const uri: string = process.env.MONGODB_URI || '';
+const uri: string = process.env.MONGODB_URI || "";
 
 export const connectDB = async () => {
-    mongoose
-        .connect(uri)
-        .then(() => {
-            if (process.env.NODE_ENV === 'development') {
-                console.log('Connected to db');
-                mongoose.set('debug', true);
-            }
-        })
-        .catch((error) => {
-            console.log("Error connecting to mongo DB");
-            console.log(error);
-        });
-
+  mongoose
+    .connect(uri)
+    .then(() => {
+      if (process.env.NODE_ENV === "development") {
+        console.log("Connected to db");
+        mongoose.set("debug", true);
+      }
+    })
+    .catch((error) => {
+      console.log("Error connecting to mongo DB");
+      console.log(error);
+    });
 };

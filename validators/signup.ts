cff503f32ca28d3b0408b5c2bdef2fe03379f091
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-03 13:40:12 +0100

    add signup validator

--- validators/signup.ts
+++ validators/signup.ts
@@ -3,7 +3,7 @@ import Joi, { ValidationResult } from "joi";
 
 const Schema = Joi.object({
   username: Joi.string().min(2).max(255).required(),
-  email: Joi.string().min(5).max(255).required(),
+  email: Joi.string().email().min(5).max(255).required(),
   phone_no: Joi.string().min(10).max(20).required(),
   password: Joi.string().min(8).max(255).required(),
 });

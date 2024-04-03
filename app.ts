125938ddae7dd8bdfd803068d6896e12e46592a4
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-03 13:41:01 +0100

    add routehandler for login

--- app.ts
+++ app.ts
@@ -1,5 +1,9 @@
 import signupRouter from "./routes/signup";
+import loginRouter from "./routes/login";
 import { Express } from "express";
+
 export const RouteHandler = async (app: Express) => {
   app.use("/api/signup", signupRouter);
+  app.use("/api/login", loginRouter);
+
 };

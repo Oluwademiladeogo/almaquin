a902d3201ce24f8f950413d54632cb25e479f6b2
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-04 20:39:15 +0100

    restructure to handle everything from login controller

--- routes/login.ts
+++ routes/login.ts
@@ -3,19 +3,6 @@ import { loginController } from "../controllers/login";
 
 const router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
-    
-  if (!req.cookies?.authToken) {
-    const data: any = await loginController(req.body);
-    if (data.token) {
-      res
-        .cookie("authToken", data.token)
-        .status(data.status)
-        .json({ message: data.message });
-    } else {
-      res.status(data.status).json({ message: data.message });
-    }
-  }
-});
+router.post("/", loginController);
 
 export default router;

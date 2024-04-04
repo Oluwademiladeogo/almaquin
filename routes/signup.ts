43bce560f72d57754901444506118f14c21aa5cd
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-04 20:32:07 +0100

    restructure to handle everything from signup controller

--- routes/signup.ts
+++ routes/signup.ts
@@ -1,11 +1,8 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import { signupController } from "../controllers/signup";
 
 const router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
-  const { status, message } = await signupController(req.body);
-  res.status(status).json(message);
-});
+router.post("/", signupController);
 
 export default router;

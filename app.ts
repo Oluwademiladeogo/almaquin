c69c8e896e60659537dc5c740055e66936d6a1b1
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-04 09:35:02 +0100

    add logout functionality

--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
 import signupRouter from "./routes/signup";
 import loginRouter from "./routes/login";
 import { Express } from "express";
+import logoutRouter from "./routes/logout";
 
 export const RouteHandler = async (app: Express) => {
   app.use("/api/signup", signupRouter);
   app.use("/api/login", loginRouter);
-
+  app.use("/api/logout", logoutRouter);
 };

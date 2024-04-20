d3e5b8c32bf8674c7cce2e917393322daf6ea307
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-20 08:31:09 +0100

    change

--- routes/overview.ts
+++
@@ -1,8 +0,0 @@
-// routes/overview.ts
-import { Router } from "express";
-import { getAllUniNamesController } from "../controllers/university";
-const router = Router();
-
-router.get("/", getAllUniNamesController);
-
-export default router;

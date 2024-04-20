a604836b0f315134f83b9c15174fe642beaeca89
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-20 11:12:07 +0100

    add faq route

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import { Express } from "express";
 import logoutRouter from "./routes/logout";
 import universityRouter from "./routes/university";
 import userRouter from "./routes/user";
+import faqRouter from "./routes/faq";
 
 export const RouteHandler = async (app: Express) => {
   app.use("/api/signup", signupRouter);
@@ -11,4 +12,6 @@ export const RouteHandler = async (app: Express) => {
   app.use("/api/logout", logoutRouter);
   app.use("/api/university", universityRouter);
   app.use("/api/user", userRouter);
+  app.use("/api/faq", faqRouter);
+
 };

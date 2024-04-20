5655fd69b77aaf646456d94e33ffbb154d9a2f39
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-20 09:29:35 +0100

    update contactInfo

--- controllers/contactInfo.ts
+++ controllers/contactInfo.ts
@@ -19,12 +19,29 @@ export const addContactInfo = async (req: Request, res: Response) => {
   try {
     const { phone, email, fax } = req.body;
 
-    const newContact = new Contact({ phone, email, fax });
+    const newContact = new ContactInfo({ phone, email, fax });
     await newContact.save();
 
     res.status(201).json({ message: "Contact information added successfully" });
   } catch (error) {
     console.error("Error adding contact information:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const updateContactInfo = async (req: Request, res: Response) => {
+  try {
+    const { phone, email, fax } = req.body;
+
+    const updatedContact = await Contact.findOneAndUpdate({}, { phone, email, fax }, { new: true });
+
+    if (!updatedContact) {
+      return res.status(404).json({ error: "Contact information not found" });
+    }
+
+    res.status(200).json({ message: "Contact information updated successfully" });
+  } catch (error) {
+    console.error("Error updating contact information:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };

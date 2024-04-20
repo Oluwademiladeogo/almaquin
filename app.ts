17125447ea30fc1546d95e56bc8f320e5822184d
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-20 08:27:29 +0100

    modified route handler

--- app.ts
+++ app.ts
@@ -2,7 +2,6 @@ import signupRouter from "./routes/signup";
 import loginRouter from "./routes/login";
 import { Express } from "express";
 import logoutRouter from "./routes/logout";
-import overviewRouter from "./routes/overview";
 import universityRouter from "./routes/university";
 import userRouter from "./routes/user";
 
@@ -10,7 +9,6 @@ export const RouteHandler = async (app: Express) => {
   app.use("/api/signup", signupRouter);
   app.use("/api/login", loginRouter);
   app.use("/api/logout", logoutRouter);
-  app.use("/api/overview", overviewRouter);
   app.use("/api/university", universityRouter);
   app.use("/api/user", userRouter);
 };

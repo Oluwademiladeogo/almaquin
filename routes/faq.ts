373033d4d0fdb55d9f50e97b5664ac25473c1c39
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-20 11:11:29 +0100

    bug fix

--- routes/faq.ts
+++ routes/faq.ts
@@ -2,9 +2,9 @@ import { Router } from "express";
 import { ensureAdmin } from "../middlewares/auth";
 import { addFAQs, deleteFAQs, getFAQs, updateFAQs } from "../controllers/faq";
 const router = Router();
-router.get("/faq", getFAQs);
-router.post("/faq", ensureAdmin, addFAQs);
-router.put("/faq", ensureAdmin, updateFAQs);
-router.delete("/faq", ensureAdmin, deleteFAQs);
+router.get("/", getFAQs);
+router.post("/", ensureAdmin, addFAQs);
+router.put("/", ensureAdmin, updateFAQs);
+router.delete("/", ensureAdmin, deleteFAQs);
 
 export default router;

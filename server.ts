2a6e1e9a441af434862609ca726ec079c349be6d
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-20 09:57:45 +0100

    code refactor

--- server.ts
+++ server.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import { config } from "dotenv";
-const app = express();
-config();
-const port = process.env.PORT || 3000;
 import { connectDB } from "./startup/db";
 import { RouteHandler } from "./app";
+import cookieParser from "cookie-parser";
+
+const app = express();
+config();
+
 connectDB();
 app.use(express.json());
-const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 RouteHandler(app);
+
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });

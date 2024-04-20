47f17276c6a330882b019d9482268438b9932468
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-20 09:58:04 +0100

    optimised

--- controllers/login.ts
+++ controllers/login.ts
@@ -2,16 +2,23 @@ import { createLoginToken } from "../helpers/createLoginToken";
 import { Request, Response } from "express";
 
 export const loginController = async (req: Request, res: Response) => {
-  if (!req.cookies?.authToken) {
-    const data: any = await createLoginToken(req.body);
+  try {
+    if (!req.cookies?.authToken) {
+      const data: any = await createLoginToken(req.body);
 
-    if (data.token) {
-      res
-        .cookie("authToken", data.token)
-        .status(data.status)
-        .json({ message: data.message });
+      if (data.token) {
+        res
+          .cookie("authToken", data.token, { httpOnly: true, secure: true }) // Set cookie as httpOnly and secure
+          .status(data.status)
+          .json({ message: data.message });
+      } else {
+        res.status(data.status).json({ message: data.message });
+      }
     } else {
-      res.status(data.status).json({ message: data.message });
+      res.status(400).json({ message: "User already logged in" });
     }
+  } catch (error) {
+    console.error("Error in login controller:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };

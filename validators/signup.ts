53d491397b7a29660db95d2a13a5dfbdbdd5fb39
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-20 09:49:26 +0100

    signup update

--- validators/signup.ts
+++ validators/signup.ts
@@ -1,13 +1,17 @@
-import { signupUserDto } from "../dto/users";
 import Joi, { ValidationResult } from "joi";
-
+import { SignupUserDto } from "../dto/users";
 const Schema = Joi.object({
-  username: Joi.string().min(2).max(255).required(),
+  surname: Joi.string().min(2).max(255).required(),
+  firstName: Joi.string().min(2).max(255).required(),
+  birthday: Joi.date().iso().required(),
+  phoneNo: Joi.string().min(7).max(15).required(),
   email: Joi.string().email().min(5).max(255).required(),
-  phone_no: Joi.string().min(7).max(15).required(),
+  presentSchool: Joi.string().min(2).max(255).required(),
+  classLevel: Joi.string().min(1).max(50).required(),
+  reasonForJoining: Joi.string().min(2).max(255).required(),
   password: Joi.string().min(8).max(255).required(),
 });
 
-export const validate = (data: signupUserDto): ValidationResult => {
+export const validate = (data: SignupUserDto): ValidationResult => {
   return Schema.validate(data);
 };

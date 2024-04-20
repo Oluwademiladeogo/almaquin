de4e7f7fb18322149ddffaae4d1f177a27372aa9
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-20 08:56:29 +0100

    use contact info in place of contact

---
+++ controllers/contactInfo.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { ContactInfo } from "../models/contactInfo";
+
+export const getContactInfo = async (req: Request, res: Response) => {
+  try {
+    const contactInfo = await ContactInfo.findOne();
+    if (!contactInfo) {
+      return res.status(404).json({ error: "Contact information not found" });
+    }
+    res.status(200).json(contactInfo);
+  } catch (error) {
+    console.error("Error fetching contact information:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};

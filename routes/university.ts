4fdc60491be4d287c4d613d7b03f45961bc5bcd7
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-20 08:27:01 +0100

    add route for university description

--- routes/university.ts
+++ routes/university.ts
@@ -5,10 +5,12 @@ import {
   getUniversityByName,
   getAllUniversityDetails,
   updateUniversityByName,
+  getUniversityDescription,
 } from "../controllers/university";
 import { authenticateUser, ensureAdmin } from "../middlewares/auth";
 const router = Router();
 router.get("/overview", authenticateUser, getAllUniversityDetails)
+router.get("/description", authenticateUser, getUniversityDescription)
 router.get("/", authenticateUser, getUniversityByName);
 router.post("/", ensureAdmin, createUniversity);
 router.put("/", ensureAdmin, updateUniversityByName);

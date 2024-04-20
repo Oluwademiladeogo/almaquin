e0cf4346594c462799fdb05b1c9b780bc86a463e
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-20 11:02:16 +0100

    removed university dependency

--- controllers/faq.ts
+++ controllers/faq.ts
@@ -1,25 +1,15 @@
 import { Request, Response } from "express";
 import { FAQ } from "../models/faq";
 
-export const getFAQs = async (req: Request, res: Response) => {
+export const getFAQs = async (_req: Request, res: Response) => {
   try {
-    const universityName = (req.query.university as string).trim();
-
-    const faqs = await FAQ.findOne({ university: universityName });
+    const faqs = await FAQ.findOne({});
 
     if (!faqs) {
       return res.status(404).json({ error: "FAQs not found" });
     }
 
-    const { schoolsFAQs, undergraduateFAQs, postgraduateFAQs } = faqs;
-
-    const formattedFAQs = {
-      schoolsFAQs,
-      undergraduateFAQs,
-      postgraduateFAQs,
-    };
-
-    res.status(200).json(formattedFAQs);
+    res.status(200).json(faqs);
   } catch (error) {
     console.error("Error fetching FAQs:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -28,14 +18,12 @@ export const getFAQs = async (req: Request, res: Response) => {
 
 export const addFAQs = async (req: Request, res: Response) => {
   try {
-    const { university, schoolsFAQs, undergraduateFAQs, postgraduateFAQs } =
-      req.body;
+    const { undergraduateFAQs, postgraduateFAQs, schoolsFAQs } = req.body;
 
     const newFAQ = new FAQ({
-      university,
-      schoolsFAQs,
       undergraduateFAQs,
       postgraduateFAQs,
+      schoolsFAQs,
     });
     await newFAQ.save();
 
@@ -48,12 +36,11 @@ export const addFAQs = async (req: Request, res: Response) => {
 
 export const updateFAQs = async (req: Request, res: Response) => {
   try {
-    const { university, schoolsFAQs, undergraduateFAQs, postgraduateFAQs } =
-      req.body;
+    const { undergraduateFAQs, postgraduateFAQs, schoolsFAQs } = req.body;
 
     const updatedFAQ = await FAQ.findOneAndUpdate(
-      { university },
-      { schoolsFAQs, undergraduateFAQs, postgraduateFAQs },
+      {},
+      { undergraduateFAQs, postgraduateFAQs, schoolsFAQs },
       { new: true }
     );
 
@@ -68,13 +55,9 @@ export const updateFAQs = async (req: Request, res: Response) => {
   }
 };
 
-export const deleteFAQs = async (req: Request, res: Response) => {
+export const deleteFAQs = async (_req: Request, res: Response) => {
   try {
-    const universityName = (req.query.university as string).trim();
-
-    const deletedFAQ = await FAQ.findOneAndDelete({
-      university: universityName,
-    });
+    const deletedFAQ = await FAQ.findOneAndDelete({});
 
     if (!deletedFAQ) {
       return res.status(404).json({ error: "FAQs not found" });

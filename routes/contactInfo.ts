fc492448e826ca96c438c1c7311a3f78a83717b3
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-21 07:02:34 +0100

    add routed for contact info

---
+++ routes/contactInfo.ts
@@ -0,0 +1,8 @@
+import {Router} from "express"
+import { addContactInfo, getContactInfo, updateContactInfo } from "../controllers/contactInfo"
+import { ensureAdmin } from "../middlewares/auth"
+const router = Router()
+
+router.get("/", getContactInfo)
+router.post("/", ensureAdmin, addContactInfo)
+router.put("/", ensureAdmin, updateContactInfo)

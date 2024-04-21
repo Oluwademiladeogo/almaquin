e9d1404f11bdc796255ac0b5aa157ee54b6c6899
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-21 07:28:18 +0100

    export router

--- routes/usefulLinks.ts
+++ routes/usefulLinks.ts
@@ -1,9 +1,14 @@
-
-import {Router} from "express"
-import { authenticateUser, ensureAdmin } from "../middlewares/auth"
-import { addUsefulLinks, deleteUsefulLink, getUsefulLinks, updateUsefulLinks } from "../controllers/usefulLink"
-const router = Router()
-router.get("/", authenticateUser, getUsefulLinks)
-router.post("/", ensureAdmin, addUsefulLinks)
-router.put("/", ensureAdmin, updateUsefulLinks)
-router.delete("/", ensureAdmin, deleteUsefulLink)
+import { Router } from "express";
+import { authenticateUser, ensureAdmin } from "../middlewares/auth";
+import {
+  addUsefulLinks,
+  deleteUsefulLink,
+  getUsefulLinks,
+  updateUsefulLinks,
+} from "../controllers/usefulLink";
+const router = Router();
+router.get("/", authenticateUser, getUsefulLinks);
+router.post("/", ensureAdmin, addUsefulLinks);
+router.put("/", ensureAdmin, updateUsefulLinks);
+router.delete("/", ensureAdmin, deleteUsefulLink);
+export default router;

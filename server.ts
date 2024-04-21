76f0e0047ba17797b673109df1035a5ca219a38c
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-21 06:57:21 +0100

    add cors

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import { connectDB } from "./startup/db";
 import { RouteHandler } from "./app";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
+import cors from "cors"
 
 const app = express();
 config();
@@ -11,6 +12,7 @@ config();
 connectDB();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors())
 app.use(helmet());
 RouteHandler(app);
 

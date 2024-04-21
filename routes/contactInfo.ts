8cbc94248754264889a7e6de25d28ae07c9d3b3d
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-21 07:27:57 +0100

    export router

--- routes/contactInfo.ts
+++ routes/contactInfo.ts
@@ -1,8 +1,13 @@
-import {Router} from "express"
-import { addContactInfo, getContactInfo, updateContactInfo } from "../controllers/contactInfo"
-import { ensureAdmin } from "../middlewares/auth"
-const router = Router()
+import { Router } from "express";
+import {
+  addContactInfo,
+  getContactInfo,
+  updateContactInfo,
+} from "../controllers/contactInfo";
+import { ensureAdmin } from "../middlewares/auth";
+const router = Router();
 
-router.get("/", getContactInfo)
-router.post("/", ensureAdmin, addContactInfo)
-router.put("/", ensureAdmin, updateContactInfo)
+router.get("/", getContactInfo);
+router.post("/", ensureAdmin, addContactInfo);
+router.put("/", ensureAdmin, updateContactInfo);
+export default router;

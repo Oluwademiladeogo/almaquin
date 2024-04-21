31e1eb6e66fdc3626c46c06cbded4305b6dca65d
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-21 07:28:48 +0100

    add routes

--- app.ts
+++ app.ts
@@ -5,7 +5,9 @@ import logoutRouter from "./routes/logout";
 import universityRouter from "./routes/university";
 import userRouter from "./routes/user";
 import faqRouter from "./routes/faq";
-
+import userContactRouter from "./routes/userContact";
+import contactInfoRouter from "./routes/contactInfo";
+import usefulLinksRouter from "./routes/usefulLinks";
 export const RouteHandler = async (app: Express) => {
   app.use("/api/signup", signupRouter);
   app.use("/api/login", loginRouter);
@@ -13,5 +15,7 @@ export const RouteHandler = async (app: Express) => {
   app.use("/api/university", universityRouter);
   app.use("/api/user", userRouter);
   app.use("/api/faq", faqRouter);
-
+  app.use("/api/send-a-message", userContactRouter);
+  app.use("/api/contact", contactInfoRouter);
+  app.use("/api/links", usefulLinksRouter);
 };

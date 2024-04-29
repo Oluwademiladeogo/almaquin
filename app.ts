c57fc71f98a9e1f82de02603b216cb89451f2233
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-04-29 04:10:36 +0100

    update route to accommodate university schema changes

--- app.ts
+++ app.ts
@@ -4,10 +4,8 @@ import { Express } from "express";
 import logoutRouter from "./routes/logout";
 import universityRouter from "./routes/university";
 import userRouter from "./routes/user";
-import faqRouter from "./routes/faq";
 import userContactRouter from "./routes/userContact";
 import contactInfoRouter from "./routes/contactInfo";
-import usefulLinksRouter from "./routes/usefulLinks";
 import secSchoolRouter from "./routes/secSchool"
 export const RouteHandler = async (app: Express) => {
   app.use("/api/signup", signupRouter);
@@ -15,9 +13,7 @@ export const RouteHandler = async (app: Express) => {
   app.use("/api/logout", logoutRouter);
   app.use("/api/university", universityRouter);
   app.use("/api/user", userRouter);
-  app.use("/api/faq", faqRouter);
   app.use("/api/send-a-message", userContactRouter);
   app.use("/api/contact", contactInfoRouter);
-  app.use("/api/links", usefulLinksRouter);
   app.use("/api/sec-schools", secSchoolRouter)
 };

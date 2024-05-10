631d41a8b28b660c2da85163a9b885a47160ba84
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-05-10 09:01:06 +0100

    add validators for university fluidStudents, exams, fees, dates, admissions, documents

--- validators/university.ts
+++ validators/university.ts
@@ -1,38 +1,38 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 const ProgramSchema = Joi.object({
   name: Joi.string().required(),
-  certs: Joi.array().items(Joi.string())
+  certs: Joi.array().items(Joi.string()),
 });
 
 const AcademicSchema = Joi.object({
   name: Joi.string().required(),
-  programs: Joi.array().items(ProgramSchema).required()
+  programs: Joi.array().items(ProgramSchema).required(),
 });
 
 const UndergraduateSchema = Joi.object({
   name: Joi.string().required(),
-  programs: Joi.array().items(ProgramSchema).required()
+  programs: Joi.array().items(ProgramSchema).required(),
 });
 
 const PostgraduateSchema = Joi.object({
   name: Joi.string().required(),
-  programs: Joi.array().items(ProgramSchema).required()
+  programs: Joi.array().items(ProgramSchema).required(),
 });
 
 const OverviewSchema = Joi.object({
   name: Joi.string(),
-  description: Joi.string()
+  description: Joi.string(),
 });
 
 const RelevantLinksSchema = Joi.object({
   name: Joi.string().required(),
-  url: Joi.string().uri().required()
+  url: Joi.string().uri().required(),
 });
 
 const FAQSchema = Joi.object({
   question: Joi.string().required(),
-  answer: Joi.string().required()
+  answer: Joi.string().required(),
 });
 
 export const UniversitySchema = Joi.object({
@@ -45,5 +45,11 @@ export const UniversitySchema = Joi.object({
   undergraduate: Joi.array().items(UndergraduateSchema).required(),
   postgraduate: Joi.array().items(PostgraduateSchema).required(),
   relevantLinks: Joi.array().items(RelevantLinksSchema),
-  faq: Joi.array().items(FAQSchema)
+  faq: Joi.array().items(FAQSchema),
+  fluidStudents: Joi.string().required(),
+  exams: Joi.string().required(),
+  fees: Joi.string().required(),
+  dates: Joi.string().required(),
+  admissions: Joi.string().required(),
+  documents: Joi.string().required(),
 });

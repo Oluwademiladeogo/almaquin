8c73270f39b44993b02b9201bff31c4d78571586
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-05-10 09:02:30 +0100

    support for university fluidStudents, exams, fees, dates, admissions, documents

--- routes/university.ts
+++ routes/university.ts
@@ -12,19 +12,33 @@ import {
   getUniversitybyId,
   searchUniversitiesByName,
   getUniversitySchoolNames,
+  getUniversityByProgramType,
+  getUniversityFluidStudents,
+  getUniversityFees,
+  getUniversityExams,
+  getUniversityDates,
+  getUniversityAdmissions,
+  getUniversityDocuments,
 } from "../controllers/university";
 
 const router = Router();
 
 router.get("/all", getAllUniversities);
+//to be moved to a new endpoint
 router.get("/search", getUniversityNames);
-// router.get("/type/:type", getUniversitiesByProgramType);
 router.get("/:universityId/schools", getUniversitySchoolNames);
 router.get("/:universityId/faq", getUniversityFAQById);
 router.get("/:universityId/links", getUniversityLinksById);
-router.get("/:universityId", getUniversitybyId);
 router.get("/:universityId/description", getUniversityDescription);
-router.get("/", searchUniversitiesByName);
+router.get("/:universityId/fluid-students", getUniversityFluidStudents);
+router.get("/:universityId/exams", getUniversityExams);
+router.get("/:universityId/fees", getUniversityFees);
+router.get("/:universityId/dates", getUniversityDates);
+router.get("/:universityId/admissions", getUniversityAdmissions);
+router.get("/:universityId/documents", getUniversityDocuments);
+router.get("/:universityId", getUniversitybyId);
+router.get("/:universityId/:type", getUniversityByProgramType);
+router.get("/", searchUniversitiesByName); //regex option using query params
 router.post("/", ensureAdmin, createUniversity);
 router.put("/", ensureAdmin, updateUniversityByName);
 router.delete("/", ensureAdmin, deleteUniversityByName);

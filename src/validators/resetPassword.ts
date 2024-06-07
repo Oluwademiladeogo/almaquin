c4311c8b516bf3a42cb5025a857368144027bb86
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-06-08 00:57:38 +0100

    chore: format

--- src/validators/resetPassword.ts
+++ src/validators/resetPassword.ts
@@ -6,7 +6,9 @@ export interface ResetPasswordDto {
   newPassword: string;
 }
 
-export const validateResetPassword = (data: ResetPasswordDto): ValidationResult => {
+export const validateResetPassword = (
+  data: ResetPasswordDto
+): ValidationResult => {
   const schema = Joi.object({
     email: Joi.string().email().required(),
     otp: Joi.string().required(),

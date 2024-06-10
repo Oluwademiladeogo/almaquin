11fec83f910db43c9f250d446706f48e64ed1fc7
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-06-10 13:03:40 +0100

    fix: cors

--- server.ts
+++ server.ts
@@ -14,12 +14,12 @@ config();
 connectDB();
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors(corsOptions));
+app.use(cors());
 // app.options('*', cors(corsOptions));
 
 // process.env.NODE_ENV == "production" ? app.use(helmet()) : "";
 RouteHandler(app);
-app.use(errorHandler)
+app.use(errorHandler);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {

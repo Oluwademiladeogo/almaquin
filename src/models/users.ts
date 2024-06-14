3cf625fc0fa83c13516cbab5298033fdba846e4d
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-06-14 07:55:22 +0100

    update models for consistency

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,62 +1,70 @@
 import mongoose, { Document } from "mongoose";
 import { IUserDoc } from "../types/types";
+import { getHashedPassword } from "../helpers/hashPassword";
 
-export const User = mongoose.model<IUserDoc>(
-  "Users",
-  new mongoose.Schema<IUserDoc>({
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    phoneNo: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    otp: {
-      type: String,
-    },
-    role: {
-      type: String,
-      enum: ["User", "Admin"],
-      default: "User",
-    },
-    surname: {
-      type: String,
-      required: true,
-    },
-    firstName: {
-      type: String,
-      required: true,
-    },
-    birthday: {
-      type: Date,
-      required: true,
-    },
-    presentSchool: {
-      type: String,
-      required: true,
-    },
-    schoolLocation: {
-      type: String,
-      required: true,
-    },
-    classLevel: {
-      type: String,
-      required: true,
-    },
-    reasonForJoining: {
-      type: String,
-      required: true,
-    },
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
-  })
-);
+const UserSchema = new mongoose.Schema<IUserDoc>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phoneNo: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ["User", "Admin", "Superadmin"],
+    default: "User",
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  birthday: {
+    type: Date,
+    required: true,
+  },
+  presentSchool: {
+    type: String,
+    required: true,
+  },
+  schoolLocation: {
+    type: String,
+    required: true,
+  },
+  classLevel: {
+    type: String,
+    required: true,
+  },
+  reasonForJoining: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+UserSchema.pre('save', async function(next) {
+  if (this.isModified('password') || this.isNew) {
+    const value:any = await getHashedPassword(this.password);
+    this.password = value.hashedPassword
+  }
+  next();
+});
+
+export const User = mongoose.model<IUserDoc>("Users", UserSchema);
